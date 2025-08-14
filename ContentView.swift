e9747c5be3d80ef2b2c46bf620df1c0cd114ba83
Diego Anguita <tralcan@gmail.com>
2025-08-14 18:57:08 -0400

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trailStats
+//
+//  Created by Diego Anguita on 14-08-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
